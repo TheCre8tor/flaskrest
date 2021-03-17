38037bf0225671e073b87a59cdb5ec777c5e7b29
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-03-17 17:36:19 +0100

    Created an API route

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=["GET"])
+def get_post():
+    return jsonify({"Hello": "World"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
